fce7f58e2e95b0860d40d9a514c03af2a51d35ca
Julen Arostegi <jul_aro_aye@goierrieskola.org>
2026-04-23 14:39:55 +0200

    Aplikazioa galdera arruntekin eginda

--- flutt02/flutter_application_1/test/widget_test.dart
+++ flutt02/flutter_application_1/test/widget_test.dart
@@ -3,9 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_application_1/main.dart';
 
 void main() {
-  testWidgets('Kaixo mundua is shown', (WidgetTester tester) async {
+  testWidgets('start screen is shown', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    expect(find.text('Kaixo mundua'), findsOneWidget);
+    expect(find.text('Futbol Jokalarien Jokoa'), findsOneWidget);
+    expect(find.text('Jokoa hasi'), findsOneWidget);
   });
 }
